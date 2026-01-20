5601e17d269dab1081556dd9e4cadad5f01153cc
Maija Rislakki <maijarislakki@Maija-MacBook-Air.local>
2026-01-20 16:36:53 +0200

    3.16

--- backend/app.js
+++ backend/app.js
@@ -60,18 +60,7 @@ peopleSchema.set('toJSON', {
 
 const Person = mongoose.model('Person', peopleSchema)
 
-//uusi virheiden käsittelijä
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
-
-//
 
 
 app.get('/api/persons', (request, response) => {
@@ -121,38 +110,50 @@ app.get('/api/persons/:id',(request, response, next) => {
   .catch(error => next(error))
 })
 
+app.post('/api/persons',(request, response, next) => {
+    const body = request.body
 
-    const generateId = () => {
-        const maxId = persons.length > 0
-            ? Math.max(...persons.map(n => Number(n.id)))
-            : 0
-        return String(maxId + 1)
-        }
-    app.post('/api/persons',(request, response) => {
-        const body = request.body
+      if (!body.name) {
+          return response.status(400).json({error: 'name missing'
+          })
+      }
 
-        if (!body.name) {
-            return response.status(400).json({error: 'name missing'
-            })
-        }
-
-        if (!body.number) {
-            return response.status(400).json({ error: 'number missing' })
-        }
+      if (!body.number) {
+          return response.status(400).json({ error: 'number missing' })
+      }
         
-        const person = new Person({
-            name: body.name,
-            number: body.number,
+      const person = new Person({
+          name: body.name,
+          number: body.number,
         })
         
-        person.save().then(savedPerson => {
-            response.json(savedPerson)
+      person.save().then(savedPerson => {
+        response.json(savedPerson)
         })
+      .catch(error => next(error))
     })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
 
 const PORT = process.env.PORT //|| 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+app.use(unknownEndpoint)
+
+//virheiden käsittelijä
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
 
 app.use(errorHandler)
